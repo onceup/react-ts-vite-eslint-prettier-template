89613df38dc0d9fa8273db10ebdd0b26fc16602c
Denis Zaborev <chistoyo@gmail.com>
2023-08-05 23:13:15 +0200

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <main className='h-full flex justify-center'>
+      <h1 className='mt-4 text-3xl font-bold udnerline'>
+        Vite React-ts Eslint Prettier Tailwindcss
+      </h1>
+    </main>
+  );
+};
+
+export default App;
